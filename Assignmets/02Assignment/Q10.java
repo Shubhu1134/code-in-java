f4e9578c8ef9a6d107805d0a1b1816802b0a2510
Shubhu1134 <shubhu1134@gmail.com>
2025-07-16 22:30:00 +0530

    Q10: Check if year is a leap year

---
+++ Assignmets/02Assignment/Q10.java
@@ -0,0 +1,27 @@
+/* 10. Check if year is leap year or not
+*/
+
+import java.util.Scanner;
+
+class Test {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if(year % 4 == 0) {
+            if(year % 100 == 0) {
+                if(year % 400 == 0) {
+                    System.out.println("Leap year");
+                } else {
+                    System.out.println("Not a leap year");
+                }
+            } else {
+                System.out.println("Leap year");
+            }
+        } else {
+            System.out.println("Not a leap year");
+        }
+    }
+}
