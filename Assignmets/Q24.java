c31158665b1e8ca5b65a809f586a1c7ee630885f
Shubhu1134 <shubhu1134@gmail.com>
2025-07-20 22:49:20 +0530

    Q1: Print numbers 1 to 10 using while  loop

--- Assignmets/Q24.java
+++
@@ -1,4 +0,0 @@
-/*24. How many bricks each 25 cm long, 
-10 cm wide and 7.5 cm thick will be required for a wall 20 m long, 2 m high and 0.75 m thick? 
-If bricks sell at $900 per thousand what will it cost to build the wall?
-*/ 
