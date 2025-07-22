aa5d04ffbe76628947be7ea64f2c930f14592954
Shubhu1134 <shubhu1134@gmail.com>
2025-07-22 22:43:12 +0530

    S16: Print AP series -6 -3 0 3 6 9 up to N terms using while loop

--- Assignmets/03Assignment/S16.java
+++ Assignmets/03Assignment/S16.java
@@ -2,14 +2,17 @@
 
 import java.util.Scanner;
 class Test {
-    public static void main(String[] args) {
+    public static void main(String args[] ) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n terms: ");
         int n = sc.nextInt();
-        int start = -6;
-        for(int i = 0; i < n; i++) {
-            System.out.print(start + " ");
-            start += 3;
+        int i = -n ; 
+        System.out.println(-n);
+        while(i<n){
+            i= i+3;
+            System.out.println(i);
+        
         }
     }
 }
+
