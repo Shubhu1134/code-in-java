9bae27079f316b792b1d9812599629e9fae012a1
Shubhu1134 <shubhu1134@gmail.com>
2025-07-24 23:57:48 +0530

    Q12–Q14: Added programs for odd numbers up to N, reverse counting, and uppercase alphabets

--- Assignmets/03Assignment/S18.java
+++ Assignmets/03Assignment/S18.java
@@ -1,16 +1,37 @@
 // 18) 1 2 2 4 8 32 …… n terms
 
 import java.util.Scanner;
-class Test {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter n terms: ");
-        int n = sc.nextInt();
-        int a = 1;
-        System.out.print(a + " ");
-        for(int i = 1; i < n; i++) {
-            a = a * i;
+ class Test {
+    public static void main(String args []) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // user input for number of terms
+
+        int count = 1;
+        long a = 1, b = 2, c = 2;
+
+        if (n >= 1) {
             System.out.print(a + " ");
+            count++;
         }
+
+        if (n >= 2) {
+            System.out.print(b + " ");
+            count++;
+        }
+
+        if (n >= 3) {
+            System.out.print(c + " ");
+            count++;
+        }
+
+        while (count <= n) {
+            long next = b * c;
+            System.out.print(next + " ");
+            b = c;
+            c = next;
+            count++;
+        }
+
+        scanner.close();
     }
 }
