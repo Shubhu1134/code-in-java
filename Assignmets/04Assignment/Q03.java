ad0a87da91fbd89c515fa76e376bba6eb375aaf7
Shubhu1134 <shubhu1134@gmail.com>
2025-07-29 23:50:27 +0530

    04Assignment: Q03 (Right diagonal star pattern)

---
+++ Assignmets/04Assignment/Q03.java
@@ -0,0 +1,18 @@
+// Q03. Print the following pattern:
+// *
+//   *
+//     *
+//       *
+//         *
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            // print i spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            System.out.println("*");
+        }
+    }
+}
