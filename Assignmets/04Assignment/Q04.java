c130176265360b4107f08d0db9e8d1224c2ebd3d
Shubhu1134 <shubhu1134@gmail.com>
2025-08-02 23:24:43 +0530

    Q04.java: Print 5x5 star pattern with space after each row

--- Assignmets/04Assignment/Q04.java
+++ Assignmets/04Assignment/Q04.java
@@ -5,13 +5,24 @@
 // *****
 // *****
 
-public class Test {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+// class Test {
+//     public static void main(String[] args) {
+//         for (int i = 0; i < 5; i++) {
+//             for (int j = 0; j < 5; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+class Test {
+    public static void main (String args[]){
+        for (int i=0; i<5; i++){
+            for (int j= 0; j<5 ;j++){
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
