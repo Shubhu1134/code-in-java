4d74fc615afed09304a3373cec9cc0986ff1cc5f
Shubhu1134 <shubhu1134@gmail.com>
2025-08-02 23:24:04 +0530

    Q07.java: Fix binary alternating pattern with 1s and 0s in triangle

--- Assignmets/04Assignment/Q07.java
+++ Assignmets/04Assignment/Q07.java
@@ -8,7 +8,8 @@
 public class Test {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            int value = (i % 2 == 0) ? 0 : 1;
+            // int value = (i % 2 == 0) ? 0 : 1;
+            
             for (int j = 1; j <= i; j++) {
                 System.out.print(value);
             }
