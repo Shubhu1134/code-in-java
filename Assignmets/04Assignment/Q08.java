f1d285a3ce35abbb0aad72efe5b22d4a0d633914
Shubhu1134 <shubhu1134@gmail.com>
2025-08-02 23:24:09 +0530

    Q08.java: Add space after each line in growing star triangle pattern

--- Assignmets/04Assignment/Q08.java
+++ Assignmets/04Assignment/Q08.java
@@ -5,13 +5,24 @@
 // ****
 // *****
 
-public class Test {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
+//  class Test {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 5; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+class Test{
+    public static void main(String args []){
+        for(int i=1 ; i<=5; i++){
+            for(int j=1; j<=i; j++){
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
