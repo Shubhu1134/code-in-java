8666515d16235ef3e0f8cc9013fa24b6d61ee82d
Shubhu1134 <shubhu1134@gmail.com>
2025-08-02 23:23:55 +0530

    Q05.java: Add space after each line in 5x5 number grid pattern

--- Assignmets/04Assignment/Q05.java
+++ Assignmets/04Assignment/Q05.java
@@ -5,13 +5,24 @@
 // 12345
 // 12345
 
-public class Test {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {         // Rows
-            for (int j = 1; j <= 5; j++) {     // Columns
+// public class Test {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 5; i++) {         // Rows
+//             for (int j = 1; j <= 5; j++) {     // Columns
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+class Test {
+    public static void main(String args[]){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5; j++){
                 System.out.print(j);
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
