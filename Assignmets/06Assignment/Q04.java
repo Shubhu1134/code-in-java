2a5e244df081b0823d729b110ae00de68f295882
Shubhu1134 <shubhu1134@gmail.com>
2025-08-06 22:12:36 +0530

    Q.4 the array.

--- Assignmets/06Assignment/Q04.java
+++ Assignmets/06Assignment/Q04.java
@@ -1,6 +1,34 @@
-import java.util.Scanner;
+import java.i=util.Scanner;
+
 class Test{
-    public static void main(String args []){
-        Scanner sc = new Scanner(System.in)
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of an array :");
+        int n = sc.nextInt();
+
+        int [] arr = new int [n]; 
+
+        System.out.println("Enter the elements :");
+        for(int i=0; i<n; i++){
+            arr [i]= sc.nextInt();
+        
+
+        for (int i=0 ; i<n-1; i++){
+            for (int j=0 ; j<n-i-1; j++){
+                 if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+            }
+        }
+    }
+      System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+        }
     }
 }
