ee873d237f52033ff42562cacc3cfd6d9323952d
Shubhu1134 <shubhu1134@gmail.com>
2025-08-12 21:40:37 +0530

    added array folder

---
+++ Array/basic01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Test{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the size of array :");
+
+        int n = sc.nextInt();
+
+        int arr []= new int [n];
+
+        for(int i =0; i<n ; i++){
+            System.out.println
+        }
+
+    }
+}
