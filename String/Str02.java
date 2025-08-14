1cd3455d31d4700305be3bbc8dddd80d9cc80cad
Shubhu1134 <shubhu1134@gmail.com>
2025-08-14 23:21:34 +0530

    core concept and theory of java string

---
+++ String/Str02.java
@@ -0,0 +1,10 @@
+import java.util.lang;
+
+class shubh{
+    public static void main(String args[]){
+        String shubh = "shubham ";
+
+        System.out.println("Length :"+ shubh.length() );
+        System.out.println("upeper case"+ shubh.toUpperCase());
+    }
+}
