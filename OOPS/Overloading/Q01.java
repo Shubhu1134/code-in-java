fb15980275d0729db65dd88359258d80e5286fbe
Shubhu1134 <shubhu1134@gmail.com>
2025-08-16 22:52:49 +0530

    implimantation of oops concept in java

---
+++ OOPS/Overloading/Q01.java
@@ -0,0 +1,18 @@
+class Test{
+    public void m1(int ...x){
+        for(int element :x);
+        sum = sum+ element; 
+        System.out.println(sum);
+
+    }
+  
+
+  
+}
+public class Testmain{
+    public static void main(String args[]); 
+    
+    Test obj = new Test (); 
+    obj.m1(20,30,60 ); 
+}
+    
