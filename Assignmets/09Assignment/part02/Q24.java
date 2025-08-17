3d124b9124065c0dd3dbb262fff6c2fa58bdbadf
Shubhu1134 <shubhu1134@gmail.com>
2025-08-18 01:09:45 +0530

    Add solutions for Q21-Q25 (string startsWith, substring, trim, repeat chars, digit sum)

--- Assignmets/09Assignment/part02/Q24.java
+++ Assignmets/09Assignment/part02/Q24.java
@@ -1,8 +1,21 @@
-// Q24.java
+// 24. Write a Java program to create a new string repeating every character twice of a given string
 import java.util.Scanner;
 
-class Q24 {
+class Test {
     public static void main(String[] args) {
-        // TODO: solution for Q24
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            result += str.charAt(i);
+            result += str.charAt(i);
+        }
+
+        System.out.println("New string: " + result);
+
+        sc.close();
     }
 }
--- Assignmets/09Assignment/part02/Q25.java
+++ Assignmets/09Assignment/part02/Q25.java
@@ -1,8 +1,23 @@
-// Q25.java
+// 25. Write a Java program to return the sum of the digits present in the given string. If there are no digits the sum return is 0
 import java.util.Scanner;
 
-class Q25 {
+class Test {
     public static void main(String[] args) {
-        // TODO: solution for Q25
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isDigit(ch)) {
+                sum += Character.getNumericValue(ch);
+            }
+        }
+
+        System.out.println("Sum of digits: " + sum);
+
+        sc.close();
     }
 }
