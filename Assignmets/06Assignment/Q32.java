5a4f4899c45ab0d4df4a20f014764d6ac0519a78
Shubhu1134 <shubhu1134@gmail.com>
2025-08-17 22:00:51 +0530

    Added Q36 and Q37, updated sorting programs

--- Assignmets/06Assignment/Q32.java
+++ Assignmets/06Assignment/Q32.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q32 {
+public class Test  {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
