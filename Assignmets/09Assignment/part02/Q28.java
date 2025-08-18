3e602c322ea8c4405ea3eb4de0548dd55656205f
Shubhu1134 <shubhu1134@gmail.com>
2025-08-18 23:28:33 +0530

    added String assignment part 2 quetion 28 solution

--- Assignmets/09Assignment/part02/Q28.java
+++ Assignmets/09Assignment/part02/Q28.java
@@ -1,8 +1,20 @@
-// Q28.java
+// 28. Write a Java program to Swap Two Strings without Third String Variable
 import java.util.Scanner;
 
-class Q28 {
-    public static void main(String[] args) {
-        // TODO: solution for Q28
+class Test {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first string: ");
+        String s1 = sc.nextLine();
+        System.out.println("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        s1 = s1 + s2;               
+        s2 = s1.substring(0, s1.length() - s2.length());
+        s1 = s1.substring(s2.length());
+
+        System.out.println("After swapping:");
+        System.out.println("First string: " + s1);
+        System.out.println("Second string: " + s2);
     }
 }
