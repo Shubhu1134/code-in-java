33e5b8617ac29247d965c74e34ca454c464fe3df
Shubhu1134 <shubhu1134@gmail.com>
2025-08-18 23:28:01 +0530

    added String assignment part 2 quetion 27 solution

--- Assignmets/09Assignment/part02/Q27.java
+++ Assignmets/09Assignment/part02/Q27.java
@@ -1,8 +1,20 @@
-// Q27.java
+// 27. Write a Java program to Swap Two Strings
 import java.util.Scanner;
 
-class Q27 {
-    public static void main(String[] args) {
-        // TODO: solution for Q27
+class Test {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first string: ");
+        String s1 = sc.nextLine();
+        System.out.println("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        String temp = s1;
+        s1 = s2;
+        s2 = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("First string: " + s1);
+        System.out.println("Second string: " + s2);
     }
 }
