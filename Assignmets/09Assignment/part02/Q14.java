20499ebd1a84f7b5471f7d28cfd5bf0e60c7c7e9
Shubhu1134 <shubhu1134@gmail.com>
2025-08-18 23:26:50 +0530

    added String assignment part 2 quetion 14 solution

--- Assignmets/09Assignment/part02/Q14.java
+++ Assignmets/09Assignment/part02/Q14.java
@@ -1,8 +1,16 @@
-// Q14.java
+// 14. Write a Java program to get the canonical representation of the string object
 import java.util.Scanner;
 
 class Q14 {
     public static void main(String[] args) {
-        // TODO: solution for Q14
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        // Interning to get canonical representation
+        String canonical = str.intern();
+
+        System.out.println("Canonical representation: " + canonical);
     }
 }
