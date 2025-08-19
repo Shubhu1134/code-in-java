51974c5eeb665de968173bec261243a7fa658c16
Shubhu1134 <shubhu1134@gmail.com>
2025-08-19 20:58:42 +0530

    added String assignment part 2 quetion 30 solution

--- Assignmets/09Assignment/part02/Q30.java
+++ Assignmets/09Assignment/part02/Q30.java
@@ -1,8 +1,19 @@
-// Q30.java
+// 30. Write a Java program How to search a word inside a string?
 import java.util.Scanner;
 
-class Q30 {
-    public static void main(String[] args) {
-        // TODO: solution for Q30
+class Test {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the main string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Enter the word to search: ");
+        String word = sc.nextLine();
+
+        if (str.contains(word)) {
+            System.out.println("The word is present in the string.");
+        } else {
+            System.out.println("The word is NOT present in the string.");
+        }
     }
 }
