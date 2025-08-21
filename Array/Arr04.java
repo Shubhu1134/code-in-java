e10d48f759ad0ef5a4966de2d77bca12612af95e
Shubhu1134 <shubhu1134@gmail.com>
2025-08-21 20:18:50 +0530

    covering concepts of array

---
+++ Array/Arr04.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Test {
+
+public static void main (String args []){
+ Scanner sc = new Scanner(System.in);
+
+ System.out.println("Enter your number :");
+   int n = sc.nextInt(); 
+    int a= 4; 
+    int b = 3; 
+
+    public String(String s){
+        this.s= s ; 
+        
+    }
+
+    if(a>n){
+        System.out.println(" good , shubham ");
+
+    }else if (a<n){
+        System.out.println(" very good , shubham  ");
+    }
+    else {
+        System.out.println(" you are best shubham ");
+    }
+    for(int i=0;i<n; i++){
+System.out.println(" shubham is hero ");
+}
+
+}  
+
+}
+
