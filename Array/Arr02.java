e57bda4c3af0258654bd472c4c426a6c42bd910b
Shubhu1134 <shubhu1134@gmail.com>
2025-08-21 20:17:16 +0530

    covering concepts of array

---
+++ Array/Arr02.java
@@ -0,0 +1,12 @@
+class Test {
+    public static void main(String args[]){
+     
+     int arr[]= new int [5];
+     
+
+     for(int i = 0 ; i<5; i++){
+        System.out.println(arr[i]);
+        System.out.println(arr.getClass());
+     }
+    }
+}
