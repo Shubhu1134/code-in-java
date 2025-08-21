d206f630f32ab2c10ff98eed4fea1a7f740dd33e
Shubhu1134 <shubhu1134@gmail.com>
2025-08-21 20:20:16 +0530

    covering concepts of array

---
+++ Array/Arr05.java
@@ -0,0 +1,17 @@
+
+
+class Test {
+    public static void main(String[] args) {
+      
+
+    private String name ;
+    public Person(String name) { this.name = name; }
+    
+}
+public String greet() { return "Hello, " + name + "!"; }
+
+        Person a = new Person("Aarav");
+        Person b = new Person("Isha");
+        System.out.println(a.greet());
+        System.out.println(b.greet());
+    }
---
+++ Array/Arr06.java
@@ -0,0 +1,5 @@
+class Test{
+    public static void main(String args){
+        double x[]= new double[2147483647]
+    }
+}
