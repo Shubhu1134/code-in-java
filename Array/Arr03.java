db878b27a5cd90f91443c174befc77c9e655d21c
Shubhu1134 <shubhu1134@gmail.com>
2025-08-21 20:18:11 +0530

    covering concepts of array

---
+++ Array/Arr03.java
@@ -0,0 +1,24 @@
+class Test {
+    public static void main(String args[]){
+     
+     int g[]= new int [5];
+     byte a[] = new byte[2];
+     char b[]=new char[2];
+     short c[]= new short[2];
+     String d[]= new String[2];
+     boolean e[]= new boolean[2];
+     long f[]= new long[2];
+     double h[]= new double[2];
+     
+
+    
+        System.out.println(a.getClass());
+        System.out.println(b.getClass());
+        System.out.println(c.getClass());
+        System.out.println(d.getClass());
+        System.out.println(e.getClass());
+        System.out.println(f.getClass());
+        System.out.println(g.getClass());
+        System.out.println(h.getClass());
+     }
+    }
