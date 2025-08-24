76b550fd329c27d5752ec28e7685e9dbb08ae097
Shubhu1134 <shubhu1134@gmail.com>
2025-08-24 14:03:52 +0530

    added some more practice Question related to array in java

---
+++ Array/P01.java
@@ -0,0 +1,13 @@
+class Test {
+    public static void main (String args[]){
+        int arr[]= {1,2,3,4,5};
+        int n= 4;
+        int temp = arr[n] 
+         
+         int target = 9;
+        for(int i=0 ; i<n; i++){
+           
+
+        }
+    }
+}
