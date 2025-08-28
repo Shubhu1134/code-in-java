1bee7f63ed4aad0d741364a19c48ec8798633e47
Shubhu1134 <shubhu1134@gmail.com>
2025-08-28 21:27:45 +0530

    added some practice Question and their solution realated to String

---
+++ Assignmets/06Assignment/S02.java
@@ -0,0 +1,35 @@
+// Q.2 Find minimum and maximum element in array.
+import java.util.Scanner;
+class Test{
+    public static void main( String args []){
+    Scanner sc = sc.nextInt(System.in);
+    System.out.println(" enter the size of an array ");
+    int n = sc.nextInt();
+    
+    i
+
+
+    }
+}
+
+public class ArrayMinMax{
+    private int[] array;
+
+    public ArrayMinMax(int [] arr){
+        this.array =arr;
+    }
+    public Result findMinMax(){
+
+
+    }
+}
+
+class Result(){
+    private int min;
+    private int max;
+
+    public Result(int min , int max){
+        this.min= min;
+        this.max =max;
+    }
+}
