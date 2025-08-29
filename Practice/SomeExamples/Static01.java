3da971f2548bfd0cd78882003147053efd360b32
Shubhu1134 <shubhu1134@gmail.com>
2025-08-29 19:23:37 +0530

    atteched some practice solution

---
+++ Practice/SomeExamples/Static01.java
@@ -0,0 +1,19 @@
+class Test {
+ 
+        static String schoolName = rkspublicSchool; 
+        int rollNo = 21;
+
+        static void PrintName(){
+            System.out.println("school NAME : "+ schoolName);
+            System.out.println(" roll number "+ rollNo);
+        }
+        void instanceMethod(){
+            System.out.println("School name :"+ schoolName);
+            System.out.println("roll number :" +rollNo );
+        }
+}
+class TestMain{
+    public static void main(String args[]){
+        Test.
+    }
+}
