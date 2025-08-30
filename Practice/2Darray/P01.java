b0988438f61f428f55079274f498e83078dd255b
Shubhu1134 <shubhu1134@gmail.com>
2025-08-30 21:47:25 +0530

    atteched some practice solution

---
+++ Practice/2Darray/P01.java
@@ -0,0 +1,11 @@
+class Test {
+    public static void main(String args[]){
+        int [][] mat = {{1,2,3,}, {4,5,6}, {7,8,9,}};
+        int row = mat.length;
+        int col = mat[0].length;
+
+        for(int i =0 ; i<; i++){
+            for (int i )
+        }
+    }
+}
