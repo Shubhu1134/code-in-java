5caf38e2d4b3bd8c04979b32fc7cf07b797a4bf8
Shubhu1134 <shubhu1134@gmail.com>
2025-08-31 22:31:49 +0530

    atteched some practice solution

---
+++ Practice/ArrayPract/P02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class MyArray{
+    private int size;
+    private int arr[];
+
+    MyArray(int size ){
+        this.size = size;
+    }
+
+    public void inputValues(){
+        System.out.println(" enter the size of the array ");
+        int
+
+    }
+}
