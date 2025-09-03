babd4908b4b7ab6801e9aabd7155d3a3116076f4
Shubhu1134 <shubhu1134@gmail.com>
2025-09-03 21:19:07 +0530

    understand java more deepar

---
+++ Practice/List/P01.java
@@ -0,0 +1,21 @@
+class Person{
+    private String name;
+    private int age ;
+    private int weight; 
+    private int height;
+
+    public Person (String name ){
+        this.name= name ;
+        this.age =0;
+        this.weight=0;
+        this.height =0;
+
+    }
+    Public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    
+}
