1dc5b75c7b4fe46ea366f12eab6243865acaf25c
Shubhu1134 <shubhu1134@gmail.com>
2025-09-05 22:33:02 +0530

    adding some interface related practice code

---
+++ Practice/over.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+class Test{
+    public static void main (String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        
+
+        System.out.println(" enter the value ");
+        int a= sc.nextInt();
+
+
+        System.out.print(a);
+
+    }
+}
+    
