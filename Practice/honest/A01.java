c5909b100f02e6c66086b50e9e0ebd8a8942cad5
Shubhu1134 <shubhu1134@gmail.com>
2025-09-17 23:40:50 +0530

    added all assignment

---
+++ Practice/honest/A01.java
@@ -0,0 +1,11 @@
+// count vowel in a string 
+
+class CountVowels{
+    public static void main(String args[]){
+        String str = " shubham is doing by own";
+        int count 0 ; 
+
+        str = str.toLowerCase();
+        for (int i = 0 < )
+    }
+}
